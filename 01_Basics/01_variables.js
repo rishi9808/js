ae20b533ee1b8f3bd65f89c4184ea6a9f3f7e53c
Rishikesh K V <rishikeshkaroth@gmail.com>
2023-12-02 10:53:23 +0000

    variables

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,49 @@
+const id = "123"
+
+/* 
+id=10  
+console.log(id);
+
+this will give error as id is declared as constant. so its value os fixed!!
+*/
+
+let namee = "Rishi"
+var age = 20
+place = "mangad"
+
+console.table([name , age, place]);    // print in tabular format
+
+
+namee = "kesh"
+age = 0
+place = "uk"
+
+console.table([name , age , place ])
+
+
+
+/* 
+1. Always declare variables
+
+2. Always use const if the value should not be changed
+
+3. Always use const if the type should not be changed (Arrays and Objects)
+
+4. Only use let if you can't use const
+
+5. Only use var if you MUST support old browsers.
+
+
+
+var should not be used!!
+reason: scope allocation - var provide global scope whereas on case of late the scope limits within the block
+
+
+
+//scope of const
+The const declaration declares block-scoped local variables. The value of a constant can't be changed through reassignment using the assignment operator, but if a constant is an object, its properties can be added, updated, or removed.
+
+
+
+
+*/
