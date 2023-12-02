4657dcf84d3c5d196205f96f39811076a3bba48d
Rishikesh K V <rishikeshkaroth@gmail.com>
2023-12-02 11:08:05 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,39 @@
+"use strict"; //treat all JS code as new version
+
+// alert(2+3);   we are using nodejs ,not browser !!
+
+console.log(3 
+    + 3); // code readability should be high
+
+console.log("Rishi");
+
+/* Documentation
+
+1. MDN docs
+2. ECMA  (tc39.es)
+
+*/
+
+let age = 2;
+let name = "Rishi";
+let isLogged = false;
+let rank; //undefined
+let place = null; //null value
+
+/* 
+ NOTE!! : place = "" and place = null are not same !!!!
+*/
+
+// Datatypes
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => stand alone value , (treated as object in JS)!
+// undefined =>
+// symbol => unique
+
+//object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); //object
